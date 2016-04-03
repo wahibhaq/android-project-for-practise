7499bd64f358a275c644643271012df99558beac
Wahib <wahib@freeletics.com>
2016-04-04 00:18:32 +0200

    Showing population in million and billion

--- app/src/main/java/com/learning/daggertwo/casterio/fragments/CountryInfoFragment.java
+++ app/src/main/java/com/learning/daggertwo/casterio/fragments/CountryInfoFragment.java
@@ -96,7 +96,8 @@ public class CountryInfoFragment extends BaseFragment {
 
                 if (countryInfo != null && countryInfo.getPopulation() != null) {
                     int populationCount = countryInfo.getPopulation();
-                    mTxtTitle.setText("Population of " + countryName + " is : " + populationCount);
+                    mTxtTitle.setText("Population of " + countryName + " is : " +
+                            getFormattedPopulationCount(populationCount));
 //                    Log.d(TAG, "Country Population : " + populationCount);
 
                     //Adding data but not retrieving for now.
@@ -114,6 +115,28 @@ public class CountryInfoFragment extends BaseFragment {
                 Log.d(TAG, "Country info failed : " + e.getMessage());
             }
         });
+    }
+
+    //http://stackoverflow.com/a/16606231/1016544
+    private String getFormattedPopulationCount(int count) {
+        long million = 1000000L;
+        long billion = 1000000000L;
+        long trillion = 1000000000000L;
+        long number = Math.round(count);
+        if ((number >= million) && (number < billion)) {
+            float fraction = calculateFraction(number, million);
+            return Float.toString(fraction) + " Million";
+        } else if ((number >= billion) && (number < trillion)) {
+            float fraction = calculateFraction(number, billion);
+            return Float.toString(fraction) + " Billion";
+        }
+        return Long.toString(number);
+    }
+
+    public float calculateFraction(long number, long divisor) {
+        long truncate = (number * 10L + (divisor / 2L)) / divisor;
+        float fraction = (float) truncate * 0.10F;
+        return fraction;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
